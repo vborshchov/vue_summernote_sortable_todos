7e71fd799f033533f3744bcc6f3415b4fd99400a
Victor Borshchov <victor.borshchov@gmail.com>
2016-09-28 18:35:06 +0300

    Adds custom summernote component

--- app.js
+++ app.js
@@ -1,3 +1,114 @@
+var VueSummernote = Vue.extend({
+  replace: true,
+  inherit: false,
+  template: "<textarea class='form-control' :name='name'></textarea>",
+  props: {
+    thought: {
+      required: true,
+      twoWay: true
+    },
+    language: {
+      type: String,
+      required: false,
+      default: "en-US"
+    },
+    height: {
+      type: Number,
+      required: false,
+      default: 160
+    },
+    minHeight: {
+      type: Number,
+      required: false,
+      default: 160
+    },
+    maxHeight: {
+      type: Number,
+      required: false,
+      default: 800
+    },
+    name: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    airMode: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    toolbar: {
+      type: Array,
+      required: false,
+      default: function() {
+        return [
+          ["font", ["bold", "italic", "underline", "clear"]],
+          ["fontsize", ["fontsize"]],
+          ["para", ["ul", "ol", "paragraph"]],
+          ["color", ["color"]],
+          ["insert", ["link", "picture", "hr"]]
+        ];
+      }
+    }
+  },
+  beforeCompile: function() {
+    this.isChanging = false;
+    this.control = null;
+  },
+  ready: function() {
+    //  initialize the summernote
+    if (this.minHeight > this.height) {
+      this.minHeight = this.height;
+    }
+    if (this.maxHeight < this.height) {
+      this.maxHeight = this.height;
+    }
+    var me = this;
+    this.control = $(this.$el);
+    this.control.summernote({
+      lang: this.language,
+      height: this.height,
+      minHeight: this.minHeight,
+      maxHeight: this.maxHeight,
+      airMode: this.airMode,
+      toolbar: this.toolbar,
+      callbacks: {
+        onInit: function() {
+          me.control.summernote("code", me.model);
+        }
+      }
+    }).on("summernote.change", function() {
+      // Note that we do not use the "onChange" options of the summernote
+      // constructor. Instead, we use a event handler of "summernote.change"
+      // event because that I don't know how to trigger the "onChange" event
+      // handler after changing the code of summernote via ".summernote('code')" function.
+      if (! me.isChanging) {
+        me.isChanging = true;
+        var code = me.control.summernote("code");
+        me.model = (code === null || code.length === 0 ? null : code);
+        me.$nextTick(function () {
+          me.isChanging = false;
+        });
+      }
+    });
+  },
+  watch: {
+    "model": function (val, oldVal) {
+      console.log(this.thought);
+      console.log(this.thought.index);
+      if (! this.isChanging) {
+        this.isChanging = true;
+        var code = (val === null ? "" : val);
+        this.control.summernote("code", code);
+        this.isChanging = false;
+      }
+    }
+  }
+});
+
+Vue.component('vue-summernote', VueSummernote);
+
+
 Vue.directive('summernote', {
   terminal: true,
   bind: function() {
@@ -46,6 +157,10 @@ var vm = new Vue({
     });
   },
 
+  // components: {
+  //   "vue-summernote": VueSummernote
+  // },
+
   data: {
     thoughts: [
       {
@@ -69,7 +184,8 @@ var vm = new Vue({
         author: null
       }
     ],
-    newThought: ''
+    newThought: '',
+    text: "Hello world!"
   },
 
   methods: {
@@ -104,5 +220,15 @@ $.fn.extend({
         return 0;
       }
     }
+  }
+});
+
+
+// Vue.component('vue-html-editor', VueHtmlEditor);
+
+var vm_bower = new Vue({
+  el: "#app",
+  data: {
+    text: "Hello World!"
   }
 });
